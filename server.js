9079420c4001b522783c2faf169cb49f4f3a7a87
easytut2009-beep <easytut2009@gmail.com>
2026-02-15 11:19:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -267,8 +267,8 @@ ${course.title}
    ✅ START SERVER
 ============================== */
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("✅ Server Running on port " + PORT);
 });
